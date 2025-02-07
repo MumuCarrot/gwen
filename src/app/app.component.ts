37ffe195d42b61db1195672c5c3b6bb5ce5d2190
MumuCarrot <ivan.kovalenko8080@gmai.com>
2025-02-07 18:19:37 +0100

    first commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'gwen-app',
+    standalone: true,
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.scss']
+})
+export class AppComponent {}
