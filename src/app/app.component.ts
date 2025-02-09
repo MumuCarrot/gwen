35ef039f1b0bb81e776f1e846647b20bbcb7c25b
MumuCarrot <ivan.kovalenko8080@gmai.com>
2025-02-09 22:25:58 +0100

    Add animations and dimmer effect to champion display; update HttpService integration

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { HeaderComponent } from './header/header.component';
 import { ChampionService } from './champion.service';
 import { ContentComponent } from './content/content.component';
+import { HttpService } from 'src/http.service';
+import { Champion } from './content/chapmions';
+import { animate, animateChild, query, state, style, transition, trigger } from '@angular/animations';
 
 @Component({
     selector: 'gwen-app',
@@ -9,6 +12,39 @@ import { ContentComponent } from './content/content.component';
     imports: [HeaderComponent, ContentComponent],
     templateUrl: './app.component.html',
     styleUrls: ['./app.component.scss'],
-    providers: [ChampionService]
+    providers: [ChampionService, HttpService],
+    animations: [
+        trigger('fadeInOutHeader', [
+            state('true', style({ opacity: 1 })),
+            state('false', style({ opacity: 0 })),
+            transition('false => true', [
+                animate('2s 0.5s ease')
+            ])
+        ]),
+        trigger('fadeInOutBody', [
+            state('true', style({ opacity: 1 })),
+            state('false', style({ opacity: 0 })),
+            transition('false => true', [
+                animate('1s 1.5s ease-in-out'),
+                query('@listAnimation', animateChild(), { optional: true })
+            ])
+        ])
+    ]
 })
-export class AppComponent {}
+export class AppComponent implements OnInit {
+    constructor(public http: HttpService) {}
+
+    championName = "Gwen";
+    champ: Champion | undefined;
+    loaded: boolean = false;
+
+    ngOnInit(): void {
+        this.http.getData("./character.json").
+            subscribe({
+                next: (data: any) => {
+                    this.champ = data.characters.find((char) => char.name.toLowerCase() == this.championName.toLowerCase()) || null;
+                    this.loaded = true;
+                }
+        });
+    }
+}
--- src/app/content/content.component.ts
+++ src/app/content/content.component.ts
@@ -1,32 +1,38 @@
-import { Component, OnInit } from '@angular/core';
-import { HttpService } from '../../http.service';
+import { Component, Input } from '@angular/core';
 import { Champion } from './chapmions';
+import { trigger, transition, style, animate, query, stagger } from '@angular/animations';
 
 @Component({
   selector: 'content-comp',
   standalone: true,
   templateUrl: './content.component.html',
   styleUrls: ['./content.component.scss'],
-  providers: [HttpService]
+  animations: [
+    trigger('listAnimation', [
+        transition(':enter', [
+            query('.text-item', [
+                style({ opacity: 0, transform: 'translateX(40px)' }),
+                stagger(200, [
+                    animate('0.5s ease-out', style({ opacity: 1, transform: 'translateX(0px)' }))
+                ])
+            ], { optional: true })
+        ]),
+        transition(':leave', [
+            query('.text-item', [
+                stagger(100, [
+                    animate('0.3s ease-in', style({ opacity: 0, transform: 'translateY(40px)' }))
+                ])
+            ], { optional: true })
+        ])
+      ]
+    )
+  ]
 })
-export class ContentComponent implements OnInit {
-    constructor(public http: HttpService) {}
+export class ContentComponent {
 
-    championName = "Gwen";
-    champ: Champion | undefined;
-    loaded: boolean = false;
+    @Input champ: Champion | undefined;
 
     scrollToTop(): void {
         window.scrollTo({ top: 0, behavior: "smooth" });
-    }
-
-    ngOnInit(): void {
-        this.http.getData("./character.json").
-            subscribe({
-                next: (data: any) => {
-                    this.champ = data.characters.find((char) => char.name.toLowerCase() == this.championName.toLowerCase()) || null;
-                    this.loaded = true;
-                }
-        });
     }
 }
